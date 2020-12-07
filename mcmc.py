475e7f5f24e8928c558656a5c3f3559d6cf05335
marijana.markov <marijana.z.markov@gmail.com>
2020-12-07 19:01:28 +0100

    clean

--- mcmc.py
+++ mcmc.py
@@ -90,22 +90,3 @@ def uni_prop(x, frm, to, sampled=None):
     is symmetric!
     """
     return np.random.uniform(frm, to)
-
-
-# how to use:
-# from functools import partial
-# import pylab
-# from mcmc *
-# # MCMC and Gibbs Sampling, by Walsh, 2004, p.8
-# # proposal dist. is uniform (symmetric) -> metropolis
-# f = partial(inv_chi_sq, n = 5, a = 4)
-# prop = partial(uni_prop, frm=0, to = 100)
-# smpls = run_chain(metropolis, f, prop, 1, 500)
-# pylab.plot(smpls[0])
-#
-# # MCMC and Gibbs Sampling, Walsh, p. 9
-# f = partial(inv_chi_sq, n = 5, a = 4)
-# prop = partial(chi_sq, n=1))
-# smpls = run_chain(metropolis, f, prop, 1, 500)
-# pylab.plot(smpls[0])
-#
